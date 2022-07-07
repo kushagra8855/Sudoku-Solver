246cdf3bee5db9d044d4e0ac9b963563f441e9fe
kushagra8855 <87658914+kushagra8855@users.noreply.github.com>
2022-07-07 23:10:10 +0530

    update

--- main.py
+++ main.py
@@ -1,87 +1,59 @@
-def find_next_empty(puzzle):
-    #finds the next row, col on the puzzle that's not filled yet --> rep with -1
-    # return row, col tuple
+def is_valid_move(grid, row, col, number):
 
-    # keep in mind that we are using 0-8 for our indices
-    for r in range(9):
-        for c in range(9):
-            if puzzle[r][c] == -1:
-                return r, c
+    for x in range(9):
+        if grid[row][x] == number:
+            return False
 
-    return None, None #if no space in the puzzle are empty (-1)
+    for x in range(9):
+        if grid[x][col] == number:
+            return False
 
-def is_valid(puzzle, guess, row, col):
-    #figures out wheather the guess at the row/col of the puzzle is a valid guess
-    #returns true if is valid, false otherwise
-
-    #let's start with the row:
-    row_vals = puzzle[row]
-    if guess in row_vals:
-        return False
-
-    #col_vals = []
-    #for i in range(9):
-    #    col_vals.append(puzzle[i][col])
-    col_vals = [puzzle[i][col] for i in range(9)]
-    if guess in col_vals:
-        return False
-
-    # and then the square
-    # this is tricky, but we want to get where the 3x3 square starts
-    # and iterate over the 3 values in the row/column
-    row_start = (row // 3) * 3# 1 // 3 = 0, 5 // 3 = 1, ...
-    col_start = (col // 3) * 3
-
-    for r in range(row_start, row_start + 3):
-        for c in range(col_start, col_start +3):
-            if puzzle[r][c] == guess:
+    corner_row = row - row % 3
+    corner_col = col - col % 3
+    for x in range(3):
+        for y in range(3):
+            if grid[corner_row+x][corner_col + y] == number:
                 return False
 
     return True
 
-def solve_sudoku(puzzle):
-    #solve sudoku using backtracking
-    #puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
-    # return whether a solution exists
-    # mutates puzzle to be the solution
+def solve(grid, row, col):
 
-    #step1: choose somewhere on the puzzle to make a guess
-    row, col = find_next_empty(puzzle)
+    if col == 9:
+        if row == 8:
+            return True
+        row += 1
+        col = 0
 
-    #step1.1: if there's nowhere left, then we're done because we only allowed valid inputs
-    if row is None:
-        return True
+    if grid[row][col] > 0:
+        return solve(grid, row, col + 1)
 
-    # step 2: if there is a place to put a number, then make a guess between 1 and 9
-    for guess in range(1, 10):
-        #step 3: check if this is valid guess
-        if is_valid(puzzle, guess, row, col):
-            # step3.1: if this is valed, then place that guess on the puzzle
-            puzzle[row][col] = guess
-            #now recurse using this puzzle
-            #step4: recursively call our function
-            if solve_sudoku(puzzle):
+    for num in range(1, 10):
+        if is_valid_move(grid, row, col, num):
+
+            grid[row][col]= num
+            if solve(grid, row, col + 1):
                 return True
-        #step5: if not valid or if our guess does not solve the puzzle, then we need to backtrack and try a new number
-        puzzle[row][col] = -1 # reset the guess
 
-    #step6: if none of the numbers that we try work, then this puzzle is UNSOLVABLE
+        grid[row][col] = 0
+
     return False
 
+grid = [ [0, 0, 0, 0, 0, 0, 6, 8, 0],
+        [0, 0, 0, 0, 7, 3, 0, 0, 9],
+        [3, 0, 9, 0, 0, 0, 0, 4, 5],
+        [4, 9, 0, 0, 0, 0, 0, 0, 0],
+        [8, 0, 3, 0, 5, 0, 9, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 3, 6],
+        [9, 6, 0, 0, 0, 0, 3, 0, 8],
+        [7, 0, 0, 6, 8, 0, 0, 0, 0],
+        [0, 2, 8, 0, 0, 0, 0, 0, 0]]
 
-if __name__ == '__main__':
-    example_board = [
-        [3,9,-1,   -1,5,-1,    -1,-1,-1],
-        [-1,-1,-1,  2,-1,-1,    -1,-1,5],
-        [-1,-1,-1,  7,1,9,      -1,8,-1],
+if solve(grid, 0,0):
+    for i in range(9):
+        for j in range(9):
+            print(grid[i][j], end=" ")
+        print()
 
-        [-1,5,-1,   -1,6,8,   -1,-1,-1],
-        [2,-1,6,     -1,-1,3,  -1,-1,-1],
-        [-1,-1,-1,    -1,-1,-1,  -1,-1,4],
-
-        [5,-1,-1,   -1,-1,-1,    -1,-1,-1],
-        [6,7,-1,    1,-1,5,      -1,4,-1],
-        [1,-1,9,     -1,-1,-1,    2,-1,-1]
-    ]
-    print(solve_sudoku(example_board))
-    print(example_board)
+else:
+    print("No Solution")
